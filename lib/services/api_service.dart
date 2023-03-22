760ae2233181a505cbb429d29eb038fe21f77bd3
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-03-22 17:09:33 +0900

    feat : api service

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,6 +3,9 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon_model.dart';
 
+import '../models/webtoon_detail_model.dart';
+import '../models/webtoon_episode_model.dart';
+
 class ApiService {
   static const String baseUrl = "webtoon-crawler.nomadcoders.workers.dev";
   static const String today = "/today";
@@ -20,4 +23,29 @@ class ApiService {
     }
     throw Error();
   }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    var url = Uri.https(baseUrl, id, {'q': '{http}'});
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    var url = Uri.https(baseUrl, id, {'q': '{http}'});
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstances;
+    }
+    throw Error();
+  }
+
 }
